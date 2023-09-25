dbc5361390100fc593f946964e163e78257956aa
Konstantin Trushin <konstantin.trushin@gmail.com>
2023-09-25 13:08:00 +0400

    Update backup script

--- backup.sh
+++ backup.sh
@@ -2,5 +2,6 @@
 
 set -eu
 
-rsync -avrlt --progress --delete  /home/ktrushin \
-    /media/ktrushin/ktrushin_backup/home
+rsync -avrlt --progress --delete \
+  --exclude /ktrushin/Downloads \
+  /home/ktrushin /media/ktrushin/ktrushin_backup/home
