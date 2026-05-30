5ecff5edec6d5e3465395e894c8dd9328c880cc4
Konstantin Trushin <konstantin.trushin@gmail.com>
2026-05-30 21:06:46 +0400

    Add `nvim` config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,10 @@
+vim.opt.clipboard:append("unnamedplus")
+
+require('kitty-scrollback').setup({
+  {
+    paste_window = {
+      highlight_as_normal_win = true,
+    },
+    visual_selection_highlight_mode = 'kitty',
+  }
+})
