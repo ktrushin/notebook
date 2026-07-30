ff4582b4b7bc4dd538b67a51eb19511c5ccf2de0
Konstantin Trushin <konstantin.trushin@gmail.com>
2026-07-30 14:51:02 +0400

    Add the script to install Go

---
+++ programming/go/install.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+
+# Define your custom locations
+set -l goroot "$HOME/.local/go"
+set -l gopath "$HOME/.local/share/go"
+
+# Fetch latest stable version and install into custom GOROOT
+set -l gov (curl -fsSL 'https://go.dev/VERSION?m=text' | head -n1)
+and curl -fsSLO "https://go.dev/dl/$gov.linux-amd64.tar.gz"
+and mkdir -p $goroot
+and tar -C $goroot --strip-components=1 -xzf "$gov.linux-amd64.tar.gz"
+and rm "$gov.linux-amd64.tar.gz"
+
+# Persist GOROOT, GOPATH, and PATH entries as universal variables
+set -Ux GOROOT $goroot
+set -Ux GOPATH $gopath
+mkdir -p $gopath/bin
+fish_add_path $goroot/bin
+fish_add_path $gopath/bin
